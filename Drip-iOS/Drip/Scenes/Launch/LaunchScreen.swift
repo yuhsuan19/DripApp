580d20f6c5651ea88e8b56a1ae4b9de8b3e43c8a
shane.chi <yuhsuan19@gmail.com>
2024-10-19 15:57:12 +0800

    chore: add package - web3Auth, web3

---
+++ Drip-iOS/Drip/Scenes/Launch/LaunchScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreen.swift
+//  Drip
+//
+//  Created by Shane Chi on 2024/10/19.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreen()
+}
---
+++ Drip-iOS/Drip/Scenes/Launch/LaunchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LaunchViewModel.swift
+//  Drip
+//
+//  Created by Shane Chi on 2024/10/19.
+//
+
+import Foundation
