5c75a5140b2acf929fb40369a491497de77e1510
shane.chi <yuhsuan19@gmail.com>
2024-11-10 13:12:37 +0800

    feat: launch screen navi bar

--- Drip-iOS/Drip/Scenes/ChallengePool/ChallengePoolScreen.swift
+++ Drip-iOS/Drip/Scenes/ChallengePool/ChallengePoolScreen.swift
@@ -13,80 +13,76 @@ struct ChallengePoolScreen: View {
     let colors = Color.generateRandomColors(count: 10)
 
     var body: some View {
-        HStack(alignment: .center) {
-            Image(.naviBarLogo)
-                .resizable()
-                .frame(width: 80, height: 32)
-            Spacer()
-            Button(action: {
-                isPresentingProfileScreen = true
-            }) {
-                ProfileChip()
-            }
-        }
-        .padding(.horizontal, 16)
-        .frame(height: 56)
-        .frame(maxWidth: .infinity)
-        .background(.black)
-        .padding(.bottom, 0)
-
-        VStack(spacing: 10) {
-            HStack {
+        VStack(spacing: 0) {
+            HStack(alignment: .center) {
+                Image(.naviBarLogo)
+                    .resizable()
+                    .frame(width: 80, height: 32)
                 Spacer()
                 Button(action: {
                     isPresentingProfileScreen = true
                 }) {
-                    ProfileChip()
+                    Image(.userProfile)
+                        .resizable()
+                        .frame(width: 32, height: 32)
                 }
             }
-            Text("Pool Name")
-                .font(.system(size: 48, weight: .black))
-                .foregroundStyle(DripColor.main)
-                .lineLimit(1)
-                .minimumScaleFactor(0.5)
-            ScrollView(showsIndicators: false) {
-                VStack(spacing: 24) {
-                    HStack(spacing: 12) {
-                        PoolInfoChip(mainText: "30", subText: "Days Remaining")
-                        PoolInfoChip(mainText: "16", subText: "Participants")
-                        PoolInfoChip(mainText: "1,500", subText: "USDC Staked")
-                    }
-                    LazyVGrid(columns: columns, spacing: 16, content: {
-                        ForEach(colors, id: \.self) { color in
-                            NavigationLink(destination: {
-                                ChallengeDetailScreen()
-                            }) {
-                                VStack(spacing: 0) {
-                                    color.aspectRatio(1, contentMode: .fill)
-                                        .padding(.bottom, 10)
-                                    Text("Challenge Name")
-                                        .font(.system(size: 14, weight: .light))
-                                        .foregroundStyle(DripColor.mainText)
-                                        .padding(.horizontal, 12)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                        .padding(.bottom, 2)
-                                    Text("30 USDC Staked")
-                                        .font(.system(size: 12, weight: .thin))
-                                        .foregroundStyle(DripColor.subText)
-                                        .padding(.horizontal, 12)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                }
-                                .padding(.bottom, 10)
-                                .background(.white)
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                            }
+            .padding(.horizontal, 16)
+            .frame(height: 56)
+            .frame(maxWidth: .infinity)
+            .background(.black)
+            .padding(.bottom, 0)
+
+            VStack(spacing: 10) {
+                Text("Pool Name")
+                    .font(.system(size: 48, weight: .black))
+                    .foregroundStyle(DripColor.main)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                ScrollView(showsIndicators: false) {
+                    VStack(spacing: 24) {
+                        HStack(spacing: 12) {
+                            PoolInfoChip(mainText: "30", subText: "Days Remaining")
+                            PoolInfoChip(mainText: "16", subText: "Participants")
+                            PoolInfoChip(mainText: "1,500", subText: "USDC Staked")
                         }
-                    })
+                        LazyVGrid(columns: columns, spacing: 16, content: {
+                            ForEach(colors, id: \.self) { color in
+                                NavigationLink(destination: {
+                                    ChallengeDetailScreen()
+                                }) {
+                                    VStack(spacing: 0) {
+                                        color.aspectRatio(1, contentMode: .fill)
+                                            .padding(.bottom, 10)
+                                        Text("Challenge Name")
+                                            .font(.system(size: 14, weight: .light))
+                                            .foregroundStyle(DripColor.mainText)
+                                            .padding(.horizontal, 12)
+                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                            .padding(.bottom, 2)
+                                        Text("30 USDC Staked")
+                                            .font(.system(size: 12, weight: .thin))
+                                            .foregroundStyle(DripColor.subText)
+                                            .padding(.horizontal, 12)
+                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                    }
+                                    .padding(.bottom, 10)
+                                    .background(.white)
+                                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                                }
+                            }
+                        })
+                    }
                 }
+                ActionButton(title: "Create New Challenge")
+                Spacer().frame(height: 10)
             }
-            ActionButton(title: "Create New Challenge")
-            Spacer().frame(height: 10)
-        }
-        .padding(.init(top: 0, leading: 24, bottom: 0, trailing: 24))
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(DripColor.backgroundGrey.ignoresSafeArea())
-        .sheet(isPresented: $isPresentingProfileScreen) {
-            ProfileScreen()
+            .padding(.init(top: 0, leading: 24, bottom: 0, trailing: 24))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(DripColor.backgroundGrey.ignoresSafeArea())
+            .sheet(isPresented: $isPresentingProfileScreen) {
+                ProfileScreen()
+            }
         }
     }
 }
